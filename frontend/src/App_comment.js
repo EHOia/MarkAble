1c92aa8b768749a0160fe25b85a819a23dbce646
danbom <52441697+danbom@users.noreply.github.com>
2021-07-15 23:33:40 +0900

    FE update
    - App.js 코멘트 버전 따로 생성
    - 컴포넌트 생성 및 코드 정리

--- frontend/src/App_comment.js
+++ frontend/src/App_comment.js
@@ -1,25 +1,48 @@
 import React, { useState, Component } from "react";
 import axios from "axios";
 
+/* import CSS */
 import styles from "./style.module.css";
 
+/* import Components */
+// 공통
 import Title from "./components/Title";
+// import TitleDesc from "./components/TitleDesc";
+
+// scene 1
+// import CategoryOptions from "./components/CategoryOptions";
+
+// scene 2
+// import LoadingTxt from "./components/LoadingTxt";
+
+// scene 3
+// import SimilarityText from "./components/SimilarityText";
+// import SubTitle from "./components/SubTitle";
+// import SimilarityImg from "./components/SimilarityImg";
+// import SimilarityDesc from "./components/SimilarityDesc";
+// import RetryBtn from "./components/RetryBtn";
 import MostSimilarityTxt from "./components/MostSimilarityTxt";
 import ListTitle from "./components/ListTitle";
 
 function App() {
+  // mode 설정 변수
   const [mode, setMode] = useState("welcome");
+
+  // text : 현재값 / setText : 변경할 값
   const [text, setText] = useState("");
   const [code, setCode] = useState("");
 
+  // 상표명 입력받는 변수
   const processText = e => {
     setText(e.target.value);
   };
 
+  // 상품 카테고리 입력받는 변수
   const codeText = e => {
     setCode(e.target.value);
   };
 
+  // (공통) input-zone 백엔드에 데이터 주기
   const sendData = () => {
     if (code === "") {
       alert("유사군코드를 입력해주세요.");
@@ -42,9 +65,20 @@ function App() {
     }
   };
 
-  const toWelcomeChange = () => {
-    setMode("welcome");
-  };
+  // Loading Effect
+  // const Loading = () => {
+  //   return (
+  //     <div className="d-flex justify-content-center">
+  //       <div
+  //         className="spinner-border"
+  //         style={{ width: "3rem", height: "3rem" }}
+  //         role="status"
+  //       >
+  //         <span className="sr-only">Loading...</span>
+  //       </div>
+  //     </div>
+  //   );
+  // };
 
   class ResultZone extends Component {
     render() {
@@ -134,6 +168,22 @@ function App() {
     }
   }
 
+  const toWelcomeChange = () => {
+    setMode("welcome");
+  };
+
+  // setMode 때문에 RetryBtn App.js 에 배치
+  // class RetryBtn extends Component {
+  //   render() {
+  //     return (
+  //       <button className={styles.rty_btn} onClick={toWelcomeChange}>
+  //         Check Others
+  //       </button>
+  //     );
+  //   }
+  // }
+
+  // 모드 변경
   var _article = null;
   if (mode === "result") {
     _article = <ResultZone></ResultZone>;
@@ -146,18 +196,20 @@ function App() {
         <div onClick={toWelcomeChange}>
           <Title></Title>
         </div>
+        {/* <TitleDesc></TitleDesc> */}
       </div>
 
       {/* input-zone : scene1 */}
       <div className={styles.input_zone}>
         <div className={styles.category_options}>
+          {/* <CategoryOptions></CategoryOptions> */}
           <p>
             <input
               className={styles.category_input}
               type="text"
               placeholder="유사군코드 입력"
               value={code}
-              onChange={codeText}
+              onChange={codeText} // onChange는 input 안의 값이 변경될 때에 발생
             />
           </p>
         </div>
@@ -167,7 +219,7 @@ function App() {
               type="text"
               placeholder="상표명 입력"
               value={text}
-              onChange={processText}
+              onChange={processText} // onChange는 input 안의 값이 변경될 때에 발생
             />
           </p>
         </div>
@@ -179,6 +231,18 @@ function App() {
       </div>
 
       {_article}
+
+      {/* loading-zone : scene2 - 데이터 로딩 함수 찾아보기 */}
+      {/* <div className={styles.loading_zone}>
+        <div className={styles.loading_effect_zone}>
+          <Loading />
+        </div>
+        <LoadingTxt></LoadingTxt>
+        
+      </div> */}
+
+      {/* result-zone : scene3  - 데이터 (유사도 퍼센트, TOP5 or 워드클라우드 이미지 ) 백에서 받아오기 */}
+      {/* _article로 대체 */}
     </div>
   );
 }
