bab4a3fa50dc4c48bcfa307c3cc2d33f15887fa2
Andrew Lee <andrewleebcs@gmail.com>
2021-07-16 12:31:50 +0900

    removed pickle on app.py

--- backend/codes/app.py
+++ backend/codes/app.py
@@ -1,22 +1,22 @@
 from flask import Flask, json, jsonify, make_response
 from flask.json import JSONEncoder
 from pymongo import MongoClient
-from flask_restx import reqparse, Api, Resource, Namespace  # Api 구현을 위한 Api 객체 import
+from flask_restx import reqparse, Api, Resource, Namespace # Api 구현을 위한 Api 객체 import
 from flask_cors import CORS
-import pickle
 from elastic_func import search_similar_text
-app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+
+app = Flask(__name__) # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 app.config['JSON_AS_ASCII'] = False
 
-api = Api(app)  # Flask 객체에 Api 객체 등록
+api = Api(app) # Flask 객체에 Api 객체 등록
 ns = api.namespace('trademark', description= '상표 데이터 테스트 api', path='')
 
-CORS(app) #다른 포트번호에 대한 보안 제거
+CORS(app) # 다른 포트번호에 대한 보안 제거
 
 parser = reqparse.RequestParser()
 
 # db 연동
-host = "mongo_db" #단독실행시 localhost / docker실행시 mongo_db
+host = "mongo_db" # 단독실행시 localhost / docker실행시 mongo_db
 port = "27017"
 conn = MongoClient(host, int(port))
 
@@ -29,7 +29,7 @@ collect2 = db.ai
 
 collect_elastic = db.sampledata
 
-#api 구현
+# api 구현
 @ns.route('/api')
 class index(Resource):
     def get(self):
@@ -47,7 +47,6 @@ class showData(Resource):
                 isFirst = False
             else:
                 s = s + ', '+ str(result)
-        #print(s)
         return s
 
 @ns.route('/api/data_transmit')
@@ -67,7 +66,7 @@ class saveTrademark(Resource):
         
         results = collect.find_one({"title":title, "code":code})
 
-        if results != None: #아예 중복되는 데이터가 있는 경우
+        if results != None: # 아예 중복되는 데이터가 있는 경우
             print(results)
             return jsonify({
                 "status": 201,
@@ -76,7 +75,7 @@ class saveTrademark(Resource):
                 "message": "데이터 등록 성공"
             })
                 
-        else: #중복 없으면 insert
+        else: # 중복 없으면 insert
             mongo_res = collect_elastic.find({'similar_group':{"$eq": code}})
             sim_titles, scores = search_similar_text(title, mongo_res, code)
             doc = {
--- backend/codes/elastic_func.py
+++ backend/codes/elastic_func.py
@@ -2,14 +2,14 @@ import pickle
 import elasticsearch_dsl as dsl
 from elasticsearch import Elasticsearch
 
-with open('./setting.pckl', 'rb') as fil: 
+with open('./setting.pckl', 'rb') as fil:
     mapping = pickle.load(fil)
 
 def search_similar_text(query_title, mongo_res, code):
     
     index_name = code.lower()
-    es = Elasticsearch('elasticsearch:9200') 
-    if not es.indices.exists(index=index_name):    
+    es = Elasticsearch('elasticsearch:9200')
+    if not es.indices.exists(index=index_name):
         es.indices.create(index=index_name, body=mapping)
         for hit in mongo_res:
             es.index(index=index_name, body={'title':hit['title'],
@@ -17,6 +17,7 @@ def search_similar_text(query_title, mongo_res, code):
                                         'category':hit['category'],
                                         'similar_group':hit['similar_group']})
         es.indices.refresh()
+    
     S = dsl.Search(using=es, index=index_name)
     s = S.query(
         'multi_match',
