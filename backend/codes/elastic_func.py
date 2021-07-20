ede9643eafe62186e2eecd3a3105b8037547cf26
yw0nam <spow2544@gmail.com>
2021-07-20 16:26:09 +0900

    elasticsearch error fix

--- backend/codes/elastic_func.py
+++ backend/codes/elastic_func.py
@@ -1,16 +1,62 @@
-import pickle
 from elasticsearch import Elasticsearch
 from langdetect import detect
 
-with open('./setting.pckl', 'rb') as fil:
-    mapping = pickle.load(fil)
+# Define mapping
+settings = {
+  "settings": {
+    "index": {
+      "similarity": {
+        "my_similarity": {
+          "type": "BM25"
+        }
+      }
+    },
+    "analysis" :{
+        "analyzer":{
+            "nori": {
+                "tokenizer" : "nori_tokenizer"
+            }
+        }
+    },
+  },
+"mappings": {
+    "properties": {
+      "title": {
+        "type": "text",
+        "similarity": "my_similarity",
+        "fields" : {
+            "nori":{
+                "type" : "text",
+                "analyzer": "nori"
+            },
+            "english": {
+              "type": "text",
+              "analyzer": "english"
+            }
+        }
+      },
+      "category":{
+        "type":"text"   
+      },
+      "similar_group": {
+        "type": "text",
+      },
+      "enroll_num":{
+        "type": "keyword",
+      },
+    #   "enroll_statement":{
+    #     "type": "keyword",
+    #   }
+    }
+  }
+}
 
 def search_similar_text(query_title, mongo_res, code):
     
     index_name = code.lower()
     es = Elasticsearch('elasticsearch:9200')
     if not es.indices.exists(index=index_name):
-        es.indices.create(index=index_name, body=mapping)
+        es.indices.create(index=index_name, body=settings)
         for hit in mongo_res:
             es.index(index=index_name, body={'title':hit['title'],
                                         'enroll_num':hit['enroll_num'],
